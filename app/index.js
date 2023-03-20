61c37c56e7f6ae9d31ff6cdc48d05d03e9ac1fce
Debdut karpas <nilkarpas@gmail.com>
2023-03-21 01:52:45 +0530

    add search functionality

--- app/index.js
+++ app/index.js
@@ -5,6 +5,10 @@ import { COLORS, icons, images, SIZES } from "../constants"
 import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components"
 
 const Home = () => {
+	const router = useRouter();
+	const [searchTerm,setSearchTerm] = React.useState("")
+
+
 	return (
 		<SafeAreaView style={{ flex:1, backgroundColor : COLORS.lightWhite }}>
 			<Stack.Screen
@@ -27,7 +31,15 @@ const Home = () => {
 						padding : SIZES.medium
 					}}
 				>
-					<Welcome/>
+					<Welcome 
+						searchTerm={searchTerm}
+						setSearchTerm={setSearchTerm}
+						handleClick={()=>{
+							if(searchTerm){
+								router.push(`/search/${searchTerm}`)
+							}
+						}}
+					/>
 					<Popularjobs/>
 					<Nearbyjobs/>
 				</View>
